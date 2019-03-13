d6d5c4b8a800cce26100b6e1531196d0d2b8e26d
Wig Cheng <wig.cheng@technexion.com>
2019-03-13 15:51:27 +0800

    uart-test: correct the tag name of debug level

--- jni/SerialPort.c
+++ jni/SerialPort.c
@@ -25,7 +25,7 @@
 #include "SerialPort.h"
 
 #include <cutils/log.h>
-static const char *TAG="TAILYN_serial_port";
+static const char *TAG="TN_serial_port";
 #define LOGI(fmt, args...) __android_log_print(ANDROID_LOG_INFO,  TAG, fmt, ##args)
 #define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, TAG, fmt, ##args)
 #define LOGE(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, TAG, fmt, ##args)
